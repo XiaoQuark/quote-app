92b997675cee6a0be3c5cc9da264e402404cf8df
Kris Oldrini <kris.oldrini@gmail.com>
2026-09-02 12:45:01 +0200

    deploy: update URL for deployment

--- frontend/script.js
+++ frontend/script.js
@@ -6,54 +6,54 @@ const quoteInput = document.getElementById("quote-input");
 const authorInput = document.getElementById("author-input");
 const formFeedback = document.getElementById("form-feedback");
 
-const URL = "http://localhost:3000/";
+const URL = "https://63xv8ftsjbkvakfxeamuedau.trainees.hosting.cyf.academy/";
 
 async function displayQuote() {
-	const response = await fetch(URL);
-	const quote = await response.json();
+  const response = await fetch(URL);
+  const quote = await response.json();
 
-	quoteP.innerText = quote.quote;
-	authorP.innerText = quote.author;
+  quoteP.innerText = quote.quote;
+  authorP.innerText = quote.author;
 }
 
 async function postNewQuote(event) {
-	event.preventDefault();
+  event.preventDefault();
 
-	const quote = quoteInput.value.trim();
-	const author = authorInput.value.trim();
+  const quote = quoteInput.value.trim();
+  const author = authorInput.value.trim();
 
-	if (quote === "" || author === "") {
-		formFeedback.textContent = "Quote and author cannot be empty.";
-		return;
-	}
+  if (quote === "" || author === "") {
+    formFeedback.textContent = "Quote and author cannot be empty.";
+    return;
+  }
 
-	try {
-		const response = await fetch(URL, {
-			method: "POST",
-			headers: {
-				"Content-Type": "application/json",
-			},
-			body: JSON.stringify({
-				quote: quote,
-				author: author,
-			}),
-		});
+  try {
+    const response = await fetch(URL, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        quote: quote,
+        author: author,
+      }),
+    });
 
-		if (response.ok) {
-			formFeedback.textContent = "New quote created";
-		} else {
-			const message = await response.text();
-			formFeedback.textContent = message;
-		}
-	} catch (error) {
-		formFeedback.textContent =
-			"There was an error connecting to the server. Try again";
-	}
+    if (response.ok) {
+      formFeedback.textContent = "New quote created";
+    } else {
+      const message = await response.text();
+      formFeedback.textContent = message;
+    }
+  } catch (error) {
+    formFeedback.textContent =
+      "There was an error connecting to the server. Try again";
+  }
 }
 
 window.addEventListener("load", () => {
-	displayQuote();
-	newQuoteBtn.addEventListener("click", displayQuote);
+  displayQuote();
+  newQuoteBtn.addEventListener("click", displayQuote);
 
-	createQuoteForm.addEventListener("submit", postNewQuote);
+  createQuoteForm.addEventListener("submit", postNewQuote);
 });
